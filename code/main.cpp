344f8924437cab6a9ff420b5d887d9044b263a77
Dan Adrian Mirea <damirea84@gmail.com>
2024-10-16 12:11:44 +0300

    add code

--- code/main.cpp
+++ code/main.cpp
@@ -1,7 +1,73 @@
+#include <windows.h>
 #include <iostream>
+
+// Global variable to store the window class name
+const char CLASS_NAME[] = "WinAPI Hello World";
+
+// Window Procedure function to process messages
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    static RECT rcBmp{10, 10, 110, 110}; // rectangle that encloses bitmap
+    static HBRUSH brush;
+
+    switch (uMsg)
+    {
+    case WM_CREATE:
+        brush = CreateSolidBrush(RGB(50, 151, 151));
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+        // Here you can add your custom painting code
+        FillRect(ps.hdc, &rcBmp, brush);
+        EndPaint(hwnd, &ps);
+    }
+        return 0;
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
 
 int32_t main()
 {
-    std::cout << "Hello world\n";
+    // Register the window class
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = GetModuleHandle(nullptr);
+    wc.lpszClassName = CLASS_NAME;
+
+    ATOM _atom = RegisterClass(&wc);
+
+    // Create the window
+    HWND hwnd = CreateWindowEx(
+        0,                                                          // Optional window styles
+        MAKEINTATOM(_atom),                                         // Window class
+        "Sample WinAPI Window",                                     // Window text
+        WS_OVERLAPPEDWINDOW,                                        // Window style
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, // Size and position
+        nullptr,                                                    // Parent window
+        nullptr,                                                    // Menu
+        GetModuleHandle(nullptr),                                   // Instance handle
+        nullptr                                                     // Additional application data
+    );
+
+    if (hwnd == nullptr)
+    {
+        return 0; // Window creation failed
+    }
+
+    ShowWindow(hwnd, SW_SHOW); // Show the window
+
+    // Message loop
+    MSG msg = {};
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
     return 0;
 }
